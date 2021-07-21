12f7f8cdcc71253cbf7930d499247b5a75aabb8e
michodgs25 <michodgs@gmail.com>
2021-07-21 18:11:47 +0000

    Create weather database& linkup API

--- app.py
+++ app.py
@@ -1,27 +1,53 @@
 import os
+import requests
+import sqlite3
 from flask import (
-    Flask, render_template, redirect, requests, session, url_for)
+    Flask, render_template, redirect, request, session, url_for)
 from flask_sqlalchemy import SQLAlchemy
-from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
-
 app = Flask(__name__)
-app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
-app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-app.secret_key = os.environ.get("SECRET_KEY")
-mongo = PyMongo(app)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
+app.config["API_URL"] = os.environ.get("API_URL")
+
+db = SQLAlchemy(app)
 
 
-@app.route("/")
-@app.route("/weather")
-def get_weather():
-    """Define domain pathway to homepage.
-    Args:
-    get index: define opening page of the platform.
-    Returns:
-    the weather.html document.
-    """
-    return render_template("weather.html")
+class City(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        new_city = request.form.get('city')
+
+        if new_city:
+            new_city_obj = City(name=new_city)
+
+            db.session.add(new_city_obj)
+            db.session.commit()
+
+    cities = City.query.all()
+
+    url = 'API_URL'
+
+    weather_data = []
+
+    for city in cities:
+
+        r = requests.get(url.format(city.name)).json()
+
+        weather = {
+            'city': city.name,
+            'temperature': r['main']['temp'],
+            'description': r['weather'][0]['description'],
+            'icon': r['weather'][0]['icon'],
+        }
+
+        weather_data.append(weather)
+
+    return render_template("weather.html", weather_data=weather_data)
